a84247ae9d18dac6ee725f84d5afd303fc82c7bc
Ahmed Abdullah Shourav <aa.shourav23@gmail.com>
2026-01-02 06:50:54 +0600

    Reapply "gamam123104"
    This reverts commit 171618146272a1eef923a64581ebd90f9eb506cd.

--- src/components/GamamCategory.js
+++ src/components/GamamCategory.js
@@ -8,61 +8,61 @@ import { useNavigate } from "react-router-dom";
 function GamamCategory({ categoryName }) {
   const navigate = useNavigate();
 
-    const [problemsList, setProblemsList] = useState([]);
-    const [problemStatusToggle, setProblemStatusToggle] = useState(false);    
+  const [problemsList, setProblemsList] = useState([]);
+  const [problemStatusToggle, setProblemStatusToggle] = useState(false);
 
-    const fetchCodingProblems = useCallback(() => {
+  const fetchCodingProblems = useCallback(() => {
     setProblemsList(gamamJson[categoryName]);
-}, [categoryName]); 
-    
-    useEffect(() => {
-        fetchCodingProblems();
-    }, [fetchCodingProblems]);
+  }, [categoryName]);
+
+  useEffect(() => {
+    fetchCodingProblems();
+  }, [fetchCodingProblems]);
 
 
-    const isProblemSolved = (problemName) => {
-      const status = localStorage.getItem(problemName);
-      return (status === 'true');
-    }
+  const isProblemSolved = (problemName) => {
+    const status = localStorage.getItem(problemName);
+    return (status === 'true');
+  }
 
-    const updateProblemStatus = (problemName) => {
-      const status = localStorage.getItem(problemName);
-      const newStatus = status === "true" ? "false" : "true";
-      console.log('New Status:', newStatus);
-      localStorage.setItem(problemName, newStatus);
-      setProblemStatusToggle(!problemStatusToggle);
-    };
-    const totalSolved = problemsList.filter(problem => localStorage.getItem(problem.name) === 'true').length;
+  const updateProblemStatus = (problemName) => {
+    const status = localStorage.getItem(problemName);
+    const newStatus = status === "true" ? "false" : "true";
+    console.log('New Status:', newStatus);
+    localStorage.setItem(problemName, newStatus);
+    setProblemStatusToggle(!problemStatusToggle);
+  };
+  const totalSolved = problemsList.filter(problem => localStorage.getItem(problem.name) === 'true').length;
 
   return (
     <div className="gamam-tracker">
-        <span  style={{'fontSize': "20px", 'color': 'seagreen'}} onClick={() => navigate('/')}><i class="fa fa-home" aria-hidden="true"></i></span>
-        <h1 className="title">{categoryName}</h1>
-      
-        <div className="summary-row">
-          <div className="summary-item right">
-            <span>Total Solved</span>
-            <strong>{totalSolved}</strong>
+      <span style={{ 'fontSize': "20px", 'color': 'seagreen' }} onClick={() => navigate('/')}><i class="fa fa-home" aria-hidden="true"></i></span>
+      <h1 className="title">{categoryName}</h1>
+
+      <div className="summary-row">
+        <div className="summary-item right">
+          <span>Total Solved</span>
+          <strong>{totalSolved}</strong>
+        </div>
+      </div>
+
+      <div className="problem-list">
+        {problemsList.map((problem, problemNo) => (
+          <div key={problem.name} className="problem-row">
+            <span className="problem-no">{problemNo + 1}</span>
+            <a className="problem-url" href={problem.url} target="_blank" rel="noopener noreferrer">{problem.name}</a>
+
+            <div className="problem-difficulty">
+              {problem.difficulty && <span className={`${problem.difficulty === '(Easy)' ? "problem-difficulty-easy" : (problem.difficulty === '(Medium)' ? 'problem-difficulty-medium' : 'problem-difficulty-hard')}`}>{problem.difficulty}</span>}
+            </div>
+
+            <span className={`problem-status ${isProblemSolved(problem.name) ? "problem-solved" : "problem-unsolved"}`}
+              onClick={() => updateProblemStatus(problem.name)}>
+              {isProblemSolved(problem.name) ? "Solved" : "Unsolved"}
+            </span>
           </div>
-        </div>
-
-        <div className="problem-list">
-            {problemsList.map((problem, problemNo) => (
-                <div className="problem-row">
-                    <span className="problem-no">{problemNo + 1}</span>
-                    <a className="problem-url" href={problem.url} target="_blank" rel="noopener noreferrer">{problem.name}</a>
-                    
-                    <div className="problem-difficulty">
-                    {problem.difficulty && <span className={`${problem.difficulty === '(Easy)' ? "problem-difficulty-easy" : (problem.difficulty === '(Medium)' ? 'problem-difficulty-medium' : 'problem-difficulty-hard')}`}>{problem.difficulty}</span>}
-                    </div>
-
-                    <span className={`problem-status ${isProblemSolved(problem.name) ? "problem-solved" : "problem-unsolved"}`} 
-                    onClick={() => updateProblemStatus(problem.name)}>
-                    {isProblemSolved(problem.name) ? "Solved" : "Unsolved"}
-                    </span>
-                </div>
-            ))}
-        </div>
+        ))}
+      </div>
     </div>
   );
 }
